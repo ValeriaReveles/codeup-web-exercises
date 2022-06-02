545b56ec48588c00c2a81ae01339a6646bd69dbf
Valeria <valeriareveles12@gmail.com>
2022-06-02 06:41:25 -0500

    updated external.js

--- js/external.js
+++ js/external.js
@@ -3,3 +3,14 @@ alert("Welcome to my Website!");
 let userInput = prompt('What is your favorite color?');
 console.log('The user entered: ' + userInput);
 alert("Great, " + userInput + " is my favorite color too.!");
+let Brother_Bear = prompt("How Many days do you want to rent the movie Brother Bear?", 5);
+let Little_Mermaid = prompt("How Many days do you want to rent the movie Little Mermaid?", 3);
+let Hercules = prompt("How Many days do you want to rent the movie Hercules?", 1);
+let google_h = prompt("How many hours did you work this week for Google ? ", 6),
+    amazon_h = prompt("How many hours did you work this week for amazon ?", 4),
+    facebook_h = prompt("How many hours did you work this week for facebook ?", 10);
+let timeFree = prompt("how many hours of learning can you do ? ", (Math.round(Math.random() * 50)));
+let bought = prompt("How many items are you going to buy ? ", (Math.round(Math.random() * 10)));
+let member = prompt("are you a member ?", "yes");
+let pro1 = prompt("type in username",'codeup');
+let pro2 = prompt("type in your password",'notastrongpassword');
