fa5c0e7471dcf7836bd0bad012940c89ff902dab
Valeria <valeriareveles12@gmail.com>
2022-06-07 10:56:57 -0500

    break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,25 @@
-do {
-    let randomOddNumber = prompt("Hello! Please pick an odd number between 1 and 50.");
+function isOdd(n){
+    return n % 2 !== 0;
+}
 
-    if(isOdd(userNumber) && isNumeric(userNumber) && userNumber >= 1 && userNumber <= 50) {
+function chosenNumber (n) {
+    return !isNaN(parseFloat(n));
+}
+
+var randomUserNumber;
+
+do {
+    let randomUserNumber = prompt("Hello! Please pick an odd number between 1 and 50.");
+
+    if(isOdd(randomUserNumber) && chosenNumber(randomUserNumber) && randomUserNumber >= 1 && randomUserNumber <= 50) {
         break;
     }
 } while(true);
+
+for(var i = 1; i <= 50; i+=2) {
+    if(i === randomUserNumber) {
+        console.log("Yikes! Skipping number: " + randomUserNumber);
+        continue;
+    }
+    console.log("Here's an odd number " + i);
+}
