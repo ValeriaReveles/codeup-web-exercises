30597574ed291a97fe00d3336599b8c0d880de5d
Valeria <valeriareveles12@gmail.com>
2022-06-08 10:35:50 -0500

    completed for loops and break and continue exercises with instructor

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,19 +2,18 @@
 // // Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 // //Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 
-var number;  //need to declare the variables before you can use it.
+var number;
 var i;
 
-// Correct and better while solution from reading and looking at examples
 
-while (true) { //If condition is true aka user entered an odd number between 1 and 50, "break" the cycle/loop of asking for a prompt. If user enters an even number, the loop will continue prompting for number.
+// Correct and better while solution reviewed with instructor:
 
-    var number = prompt("Hello! Enter an odd number between 1 and 50.")
+while (true) {
+    var number = prompt("Hello! Please enter an odd number between 1 and 50: ")
 
     if (number % 2 !== 0 && number >= 1 && number <= 50)
         break;
 }
-
 
 for (i = 1; i <= 50; i++) {
     if (number == i) {
@@ -29,29 +28,3 @@ for (i = 1; i <= 50; i++) {
 
 }
 
-
-//Other possible working solution. Though not as clean and effective. Very repetitive. Work on cleaning code and making it easier to read.
-
-
-// function userNumber (n){
-//     return  n % 2 !== 0
-// }
-//
-// function chosenNumber (n) {
-//     return !isNaN(parseFloat(n));
-// }
-//
-//
-// let enterNumber = prompt("Hello there! Please enter an odd number between 1 and 50");
-//
-// for (var i = 1; i <= 50; i+=2){
-//     if(i === enterNumber) {
-//         console.log("Yikes! Skipping number: " + enterNumber);
-//         continue;
-//     }
-//      console.log("Here's an odd number " + i);
-//  }
-//
-// if(userNumber(enterNumber) && chosenNumber(enterNumber) && enterNumber >= 1 && enterNumber <= 50){
-//     break;
-// }
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //7 x 1 = 7
 // 7 x 2 = 14
 // 7 x 3 = 21
@@ -11,26 +13,29 @@
 
 //Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
 //
-
 function showMultiplicationTable (number){
-    for(let i = 1; i <11; i++){
+    for(let i = 1; i <11; i++){ //could use i <=10 instead
         console.log(number + " x " + i + " = " + (number * i));
     }
 }
 
-//Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
 
+//Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
    //can create a random function to use for all random number between min and max
+    //can declare it inside the loop
 }
-
-// var message;
-// var number; don't need to repetitive with your code
-
+//Notes on how instructor did it.
 for(var i = 0; i < 10; i++) {
- let number = getRandomInt(20, 200);
- let message = "";
+    let number = getRandomInt(20, 200); //let getRandomIt =  Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+    //if(getRandomInt% 2 === 0){
+    //console.log(getRandomInt( + "!Nice, that is an even number."))
+//}else {
+    //console.log(getRandomInt + "! Odd number, cool!"));
+    //}
+//}
+    let message = "";
     if(number % 2 === 0) {
         message = " is even";
     } else {
@@ -42,16 +47,16 @@ for(var i = 0; i < 10; i++) {
 }
 
 
-//Create a for loop that uses console.log to create the output shown below.
-// 1
-// 22
-// 333
-// 4444
-// 55555
-// 666666
-// 7777777
-// 88888888
-// 999999999
+// //Create a for loop that uses console.log to create the output shown below.
+// // 1
+// // 22
+// // 333
+// // 4444
+// // 55555
+// // 666666
+// // 7777777
+// // 88888888
+// // 999999999
 
 var output;
 for(var i = 1; i <= 9; i++) {
@@ -61,13 +66,19 @@ for(var i = 1; i <= 9; i++) {
     }
     console.log(output)
 }
+//Alternate: can use repeat to make it simple and avoid nesting and make it very simple.
+
+//for (let i =1; i <= 9; i++){
+    //let iAsString = i.toString();
+    //console.log(i.toString().repeat(i));
+//}
 
 
 
-
-//Create a for loop that uses console.log to create the output shown below.
-for(var i = 100; i >= 5; i-=5) {
+// //Create a for loop that uses console.log to create the output shown below.
+for(var i = 100; i >= 0; i-=5) {
     console.log(i);
 }
 
-
+//
+//
